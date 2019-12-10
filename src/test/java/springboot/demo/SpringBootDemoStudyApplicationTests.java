04ae37f6bf6d5b44688a9baf71043a8b9d2f74ef
hanqing <hqhanqing@foxmail.com>
2019-12-10 15:45:44 +0800

    修改测试类名

--- src/test/java/springboot/demo/SpringBootDemoStudyApplicationTests.java
+++ src/test/java/springboot/demo/SpringBootDemoStudyApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootDemoStudyApplicationTests {
+class SpringBootDemoApplicationTests {
 
     @Test
     void contextLoads() {
