5a7a3b82672914ad344f0037a00016ab19406815
hanqing <hqhanqing@foxmail.com>
2019-12-10 12:30:45 +0800

    first commit

---
+++ src/main/java/springboot/demo/SpringBootDemoStudyApplication.java
@@ -0,0 +1,14 @@
+package springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoStudyApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringBootDemoStudyApplication.class, args);
+    }
+
+}
