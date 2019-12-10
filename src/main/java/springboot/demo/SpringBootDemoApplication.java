adab62a3f81d9e0c63eccd388d32ad5ce60311df
hanqing <hqhanqing@foxmail.com>
2019-12-10 15:44:25 +0800

    修改类名

--- src/main/java/springboot/demo/SpringBootDemoApplication.java
+++ src/main/java/springboot/demo/SpringBootDemoApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootDemoStudyApplication {
+public class SpringBootDemoApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(SpringBootDemoStudyApplication.class, args);
+        SpringApplication.run(SpringBootDemoApplication.class, args);
     }
 
 }
