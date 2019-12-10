f0057de00a37705be53c242956e64d449e31b887
hanqing <hqhanqing@foxmail.com>
2019-12-10 19:18:28 +0800

    增加部分html

--- src/test/java/springboot/demo/SpringBootDemoApplicationTests.java
+++ src/test/java/springboot/demo/SpringBootDemoApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootDemoApplicationTests {
+public class SpringBootDemoApplicationTests {
 
     @Test
     void contextLoads() {
